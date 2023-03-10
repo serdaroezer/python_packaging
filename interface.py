b98dbb2643e92c413759bdbdd59af984e6baaffa
serdar özer <serdar.oezer@cgi.com>
2023-03-10 18:01:37 +0100

    feat: helper function file added

--- interface.py
+++ interface.py
@@ -3,8 +3,7 @@ import typing
 from pathlib import Path
 import click
 import time
-
-
+from models import train_polynomial_model, predict
 
 
 @click.group()
@@ -17,27 +16,37 @@ def cli() -> None:
 @click.command(name='train')
 @click.option('--p', default='RealEstate.csv', help='dataset path')
 @click.option('--d', default=2, help='polynom degree')
-def train(p: str, d: int) -> None:
-    with click.progressbar(label='processing', length=100, show_eta=True) as progress:
-        # model_train(progress)
+@click.option('--l', default='Y house price of unit area', help='column name from dataset for label')
+@click.option('--dc', default='No', help='column name to drop from dataset, could be a list of columns')
+@click.option('--sl', default=20, help='percentage to split data to the validation and train')
+@click.option('--mn', default='house_prediction',
+              help='model name to save trained model, give it with full path. Example: /your/path/<model_name>')
+def train(p: str, d: int, l: str, sl: int, mn: str, dc ) -> None:
+    # with click.progressbar(label='processing', length=100, show_eta=True) as progress:
+    # click.pass_context(f=train_polynomial_model)
+    # with click.pass_context() as progress:
+    train_polynomial_model(data_path=p, split_rate=sl, label_name=l, polynom_degree=d, model_name=mn, dropped_columns=dc)
+
 
 @click.command(name='test')
-@click.option('--p', default='RealEstate.csv', help='dataset path')
-def test(p: str) -> None:
-    print(p)
-    click.echo('provide dataset path to test')
+@click.option('--dp', default='RealEstate.csv', help='dataset path')
+@click.option('--d', default=2, help='polynom degree that you used during training')
+@click.option('--l', default='Y house price of unit area', help='column name from dataset for label')
+@click.option('--mn', default='house_prediction',
+              help='model name to load and make prediction. Give it with full path . Example: /your/path/<model_name>')
+def test(p: str, l: str, mn: str, d: int) -> None:
+    predict(data_path=p, label_name=l, model_name=mn, polynom_degree=d)
 
 
-@click.command(name='predict')
-@click.option('--data', type=(int, int),
-              help='list of int, float or string  values of one instance. Order of data should be same as training'
-              )
-def predict(data: typing.Tuple[int]) -> None:
-    print('prediction done', data)
+# @click.command(name='predict')
+# @click.option('--data', type=(int, int),
+#               help='list of int, float or string  values of one instance. Order of data should be same as training'
+#               )
+# def predict(data: typing.Tuple[int]) -> None:
+#     print('prediction done', data)
 
 
 if __name__ == '__main__':
     cli.add_command(train)
-    cli.add_command(predict)
     cli.add_command(test)
     cli()
--- models.py
+++ models.py
@@ -1,37 +1,11 @@
 import pickle
 import pandas as pd
 from sklearn import metrics
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LassoCV
 import numpy as np
 from os import path
+from  helper import  read_data, mean_normalize ,split_test_train, polynomial_transform_train_data
 
-
-def max_min_normalize(X):
-    return (X - X.min()) / (X.max() - X.min())
-
-
-def mean_normalize(X):
-    return (X - X.mean()) / X.std()
-
-
-def read_data(path: str, label: str,dropped_columns):
-    df = pd.read_csv(path)
-    y = df['Y house price of unit area']
-    x = df.drop([label, dropped_columns], axis=1)
-    return x, y
-
-
-def split_test_train(split_rate: int, X, y):
-    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=split_rate / 100, random_state=1, shuffle=False)
-    return x_train, x_test, y_train, y_test
-
-
-def polynomial_transform_train_data(polynom_degree: int, x_train):
-    poly = PolynomialFeatures(degree=polynom_degree)
-    x_train_poly = poly.fit_transform(x_train)
-    return x_train_poly
 
 
 def train_polynomial_model(data_path: str, split_rate: int, polynom_degree: int, label_name: str, model_name: str , dropped_columns):
